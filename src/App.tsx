2e45a42f12b1454bdb974f63531b6503a1cfbed3
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-11-14 01:20:15 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className='App-link'
+          href='https://reactjs.org'
           target="_blank"
           rel="noopener noreferrer"
         >
