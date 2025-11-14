5b5f772d160d651dd802dea6f023cb208a568561
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-11-14 00:09:18 -0300

    teste

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'prettier/prettier': 'error',
+  },
+};
