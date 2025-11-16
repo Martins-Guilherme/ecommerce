64d9d3dc089411560128684f7d9f56cf7a4ad553
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-11-16 18:30:19 -0300

    refactor: add Styled Components to Header Component

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,21 +1,27 @@
 import { BsCart3 } from 'react-icons/bs';
 import './header.style.css';
+import {
+  HeaderContainer,
+  HeaderItems,
+  HeaderItem,
+  HeaderTitle,
+} from './header.styles';
 
 const Header = () => {
   return (
-    <div className="header-container">
-      <h2 className="header-title">CLUB CLOTHING</h2>
+    <HeaderContainer>
+      <HeaderTitle>CLUB CLOTHING</HeaderTitle>
 
-      <div className="header-items">
-        <div className="header-item">Explorar</div>
-        <div className="header-item">Login</div>
-        <div className="header-item">Criar conta</div>
-        <div className="header-item">
+      <HeaderItems>
+        <HeaderItem>Explorar</HeaderItem>
+        <HeaderItem>Login</HeaderItem>
+        <HeaderItem>Criar conta</HeaderItem>
+        <HeaderItem>
           <BsCart3 size={25} />{' '}
           <span style={{ fontSize: 12, marginLeft: 5 }}>5</span>
-        </div>
-      </div>
-    </div>
+        </HeaderItem>
+      </HeaderItems>
+    </HeaderContainer>
   );
 };
 
