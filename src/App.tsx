4b8ed4b683abef46bc5dbb9e3df0397251bde788
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-11-19 23:10:55 -0300

    feat: esnsure user context is updated on  onAuthStateChanged

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useContext } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 // Pages
 import HomePage from './pages/home/home.page';
 import LoginPage from './pages/login/login.page';
 import SignUpPage from './pages/sign-up/sign-up.page';
+
+// firebase config
 import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from './config/firebase.config';
+import { auth, db } from './config/firebase.config';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+
+import { UserContext } from './contexts/user.context';
 
 const App: FunctionComponent = () => {
-  onAuthStateChanged(auth, (user) => {
-    console.log(user);
+  const { currentUser, isAuthenticated, logOutUser, loginUser } =
+    useContext(UserContext);
+
+  onAuthStateChanged(auth, async (user) => {
+    const isSigningOut = isAuthenticated && !user;
+
+    if (isSigningOut) {
+      return logOutUser();
+    }
+
+    const isSigingIn = !isAuthenticated && user;
+    if (isSigingIn) {
+      const querySnapshot = await getDocs(
+        query(collection(db, 'users'), where('id', '==', user?.uid)),
+      );
+      const userFromFireStore = querySnapshot.docs[0]?.data();
+
+      return loginUser(userFromFireStore as any);
+    }
   });
   return (
     <>
