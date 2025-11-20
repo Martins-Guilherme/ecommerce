9553f3d8ac32571b41cd4e0317ae1235377b0289
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-11-19 22:51:15 -0300

    fix: ensure with a not transient props to components

--- src/components/category-item/category-item.styles.ts
+++ src/components/category-item/category-item.styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 import Colors from '../../theme/theme.colors';
 interface CategoryItemContainerProps {
-  backgroundImage: string;
+  $backgroundImage: string;
 }
 
 export const CategoryItemContainer = styled.div<CategoryItemContainerProps>`
@@ -20,7 +20,7 @@ export const CategoryItemContainer = styled.div<CategoryItemContainerProps>`
   box-shadow: 2px 2px 2px #00000040;
   background-color: #0000004d;
   background-blend-mode: color;
-  background-image: ${(props) => `url('${props.backgroundImage}')`};
+  background-image: ${(props) => `url('${props.$backgroundImage}')`};
 `;
 
 export const CategoryName = styled.div`
--- src/components/custom-input/custom-input.components.tsx
+++ src/components/custom-input/custom-input.components.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent, InputHTMLAttributes } from 'react';
 import { CustonInputContainer } from './custon-input.styles';
 
 interface CustomInput extends InputHTMLAttributes<HTMLInputElement> {
-  hasError?: boolean;
+  $hasError?: boolean;
 }
 
 const CustomInput: FunctionComponent<CustomInput> = React.forwardRef(
