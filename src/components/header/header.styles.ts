641782a4f3784d4fec2a0ff9b451d7c72672217d
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-12-17 13:55:41 -0300

    style: update header styles for responsive design

--- src/components/header/header.styles.ts
+++ src/components/header/header.styles.ts
@@ -10,6 +10,12 @@ export const HeaderContainer = styled.div`
   align-items: center;
   padding: 20px;
   color: ${Colors.text.white};
+
+  @media (max-width: 7668px) {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 `;
 
 export const HeaderTitle = styled.h2`
@@ -18,12 +24,21 @@ export const HeaderTitle = styled.h2`
   &:hover {
     cursor: pointer;
   }
+
+  @media (max-width: 768px) {
+    margin-bottom: 16px;
+  }
 `;
 
 export const HeaderItems = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    justify-content: space-evenly;
+  }
 `;
 
 export const HeaderItem = styled.div`
@@ -40,5 +55,9 @@ export const HeaderItem = styled.div`
   &:nth-child(2),
   &:nth-child(3) {
     margin-right: 40px;
+  }
+
+  @media (max-width: 768px) {
+    margin-right: 0;
   }
 `;
