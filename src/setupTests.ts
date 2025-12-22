c3797c5468ced65b3e0317ee2667016b588542e0
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-12-22 13:53:09 -0300

    chore: update dependencies in yarn.lock

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,16 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+// src/setupTests.ts
 import '@testing-library/jest-dom';
+import 'jest-styled-components';
+
+// Polyfill TextEncoder / TextDecoder para React Router 7 + Jest
+import { TextEncoder, TextDecoder } from 'util';
+
+if (typeof global.TextEncoder === 'undefined') {
+  // @ts-ignore
+  global.TextEncoder = TextEncoder;
+}
+
+if (typeof global.TextDecoder === 'undefined') {
+  // @ts-ignore
+  global.TextDecoder = TextDecoder as any;
+}
