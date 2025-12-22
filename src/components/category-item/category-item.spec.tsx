b55dd91d4a32bb451b7657cdd5902e7480d664ea
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-12-22 13:51:53 -0300

    test: add unit test for renders received category correctly

---
+++ src/components/category-item/category-item.spec.tsx
@@ -0,0 +1,25 @@
+import { render } from '@testing-library/react';
+import CategoryItem from './category-item.component';
+import Category from '../../types/category-types';
+import { BrowserRouter } from 'react-router';
+
+describe('Category Item', () => {
+  it('should render category correctly', () => {
+    const category: Category = {
+      id: '1',
+      displayName: 'Lorem Ipsum',
+      imageUrl: 'image_url',
+      name: 'lorem-ipsum',
+      products: [],
+    };
+
+    const { getByText } = render(
+      <BrowserRouter>
+        <CategoryItem category={category} />
+      </BrowserRouter>,
+    );
+
+    getByText('Lorem Ipsum');
+    getByText('Explorar')
+  });
+});
