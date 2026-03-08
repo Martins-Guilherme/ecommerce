4123b73fb6a7592bd887391d48ce23a441d33bdb
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2026-03-08 20:07:01 -0300

    fix: react-router-dom library obsoleto

--- src/components/checkout/checkout.component.tsx
+++ src/components/checkout/checkout.component.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 import axios from 'axios';
 
 import { BsAirplaneEngines, BsBagCheck } from 'react-icons/bs';
