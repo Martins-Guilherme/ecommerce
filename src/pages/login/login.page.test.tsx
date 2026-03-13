5553edd8eabe0d316c6f966a08495ec27c352a16
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2026-03-13 00:08:12 -0300

    test: ensure login shos erro if email is invalid

--- src/pages/login/login.page.test.tsx
+++ src/pages/login/login.page.test.tsx
@@ -12,4 +12,20 @@ describe('Login', () => {
     await findByText('O e-mail é obrigatório.');
     getByText('A senha é obrigatória.');
   });
+
+  it('should show error if email is invalid', async () => {
+    const { getByText, findByText, getByPlaceholderText } = renderWithRedux(
+      <LoginPage />,
+      {} as any,
+    );
+
+    const emailInput = getByPlaceholderText(/Digite seu e-mail/i);
+
+    userEvent.type(emailInput, 'invalid_email');
+
+    const submitButton = getByText('Entrar');
+
+    userEvent.click(submitButton);
+    await findByText(/por favor, insira um e-mail válido./i);
+  });
 });
